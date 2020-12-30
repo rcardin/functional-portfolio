119878185fa2e3f32d4cb52b5d5b00cbb84b0df1
rcardin <riccardo.cardin@gmail.com>
2020-12-30 15:37:27 +0100

    Added smart constructor for the Stock domain model

--- src/main/scala/in/rcard/functional/portfolio/algebra/interpreter/Portfolio.scala
+++ src/main/scala/in/rcard/functional/portfolio/algebra/interpreter/Portfolio.scala
@@ -1,7 +1,7 @@
 package in.rcard.functional.portfolio.algebra.interpreter
 
 import cats.data.Validated
-import cats.syntax.validated._
+import cats.syntax.apply._
 import in.rcard.functional.portfolio.algebra.interpreter.common.{ErrorsOr, Stocks}
 
 object common {
@@ -19,12 +19,35 @@ object Portfolio {
 final case class PortfolioId private (id: String)
 object PortfolioId {
   def create(id: String): ErrorsOr[PortfolioId] =
-    if (id != null)
-      PortfolioId(id).valid
-    else
-      List("A portfolio id cannot be null").invalid
+    Validated.cond(id != null, PortfolioId(id), List("A portfolio id cannot be null"))
 }
 
-case class Stock(id: StockId, owned: Long)
+final case class Stock private (id: StockId, owned: Long)
+object Stock {
+  def create(id: String, owned: Long): ErrorsOr[Stock] = {
+    def checkOwnedGreaterThanZero: ErrorsOr[Long] =
+      Validated.cond(
+        owned > 0,
+        owned,
+        List(s"The quantity of the stock $id cannot be less than zero, $owned")
+      )
 
-case class StockId(id: String)
+    (
+      StockId.create(id),
+      checkOwnedGreaterThanZero
+    ).mapN(Stock(_, _))
+  }
+}
+
+final case class StockId private (id: String)
+object StockId {
+  def create(id: String): ErrorsOr[StockId] = {
+    def checkNullability: ErrorsOr[String] =
+      Validated.cond(id != null, id, List("A stock id cannot be null"))
+
+    def checkStockNameValidity: ErrorsOr[String] =
+      Validated.cond(id.forall(_.isUpper), id, List("The whole stock name must be uppercase"))
+
+    checkNullability.andThen(_ => checkStockNameValidity).map(StockId.apply)
+  }
+}
