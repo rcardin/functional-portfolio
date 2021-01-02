c9eb217efb289bd94cdb1d019523b8d5b948ba3d
rcardin <riccardo.cardin@gmail.com>
2021-01-02 15:31:21 +0100

    Rewritten the interpreter of the AccountService.open function

--- src/main/scala/in/rcard/functional/portfolio/algebra/interpreter/PortfolioService.scala
+++ src/main/scala/in/rcard/functional/portfolio/algebra/interpreter/PortfolioService.scala
@@ -1,6 +1,7 @@
 package in.rcard.functional.portfolio.algebra.interpreter
 
 import cats.data.Reader
+import cats.syntax.either._
 import in.rcard.functional.portfolio.algebra.{PortfolioRepository, PortfolioService}
 import in.rcard.functional.portfolio.algebra.interpreter.common.ErrorsOr
 
@@ -10,9 +11,18 @@ object PortfolioService extends PortfolioService[Portfolio, PortfolioId, ErrorsO
   type Repository = PortfolioRepository[Portfolio, PortfolioId, ErrorsOr]
 
   override def open(id: String): Reader[Repository, ErrorsOr[Portfolio]] = Reader { repo =>
-    val portfolioId: ErrorsOr[PortfolioId] = PortfolioId.create(id)
-    portfolioId.andThen(pId => repo.findById(pId))
-    portfolioId.andThen(_ => Portfolio.emptyPortfolio(id))
-    // TODO Save to the database
+    (for {
+      pId <- PortfolioId.create(id).toEither
+      _ <- repo.findById(pId).toEither
+      portfolio <- Portfolio.emptyPortfolio(id).toEither
+      _ <- repo.store(portfolio).toEither
+    } yield portfolio).toValidated
+
+    // XXX Which is the better approach?
+//    val portfolioId: ErrorsOr[PortfolioId] = PortfolioId.create(id)
+//    portfolioId
+//      .andThen(pId => repo.findById(pId))
+//      .andThen(_ => Portfolio.emptyPortfolio(id))
+//      .andThen(repo.store)
   }
 }
