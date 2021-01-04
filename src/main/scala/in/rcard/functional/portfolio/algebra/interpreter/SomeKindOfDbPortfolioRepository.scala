0d7d5a3c821a60bba341696f566ce5a039ec180e
rcardin <riccardo.cardin@gmail.com>
2021-01-04 21:53:33 +0100

    Implemented the fake repository as a in memory mutable map

--- src/main/scala/in/rcard/functional/portfolio/algebra/interpreter/SomeKindOfDbPortfolioRepository.scala
+++ src/main/scala/in/rcard/functional/portfolio/algebra/interpreter/SomeKindOfDbPortfolioRepository.scala
@@ -1,10 +1,16 @@
 package in.rcard.functional.portfolio.algebra.interpreter
 
+import collection.mutable.{ Map => MMap}
 import in.rcard.functional.portfolio.algebra.{ErrorsOr, PortfolioRepository}
 
 object SomeKindOfDbPortfolioRepository extends PortfolioRepository[Portfolio, PortfolioId, ErrorsOr] {
 
-  override def findById(id: PortfolioId): ErrorsOr[Option[Portfolio]] = ???
+  private lazy val repo = MMap.empty[PortfolioId, Portfolio]
 
-  override def store(portfolio: Portfolio): ErrorsOr[Portfolio] = ???
+  override def findById(id: PortfolioId): ErrorsOr[Option[Portfolio]] = Right(repo.get(id))
+
+  override def store(portfolio: Portfolio): ErrorsOr[Portfolio] = {
+    repo.put(portfolio.id, portfolio)
+    Right(portfolio)
+  }
 }
