28e922b4ebfe7b04908a4bb631443a4088a020fd
rcardin <riccardo.cardin@gmail.com>
2021-01-04 22:11:24 +0100

    Added the sell function to the algebra

--- src/main/scala/in/rcard/functional/portfolio/algebra/PortfolioService.scala
+++ src/main/scala/in/rcard/functional/portfolio/algebra/PortfolioService.scala
@@ -4,4 +4,5 @@ import cats.data.Reader
 
 trait PortfolioService[Portfolio, PortfolioId, F[_]] {
   def open(id: String): Reader[PortfolioRepository[Portfolio, PortfolioId, F], F[Portfolio]]
+  def sell(idPortfolio: String, stock: String, qty: Long): Reader[PortfolioRepository[Portfolio, PortfolioId, F], F[Portfolio]]
 }
--- src/main/scala/in/rcard/functional/portfolio/algebra/interpreter/PortfolioService.scala
+++ src/main/scala/in/rcard/functional/portfolio/algebra/interpreter/PortfolioService.scala
@@ -3,6 +3,8 @@ package in.rcard.functional.portfolio.algebra.interpreter
 import cats.data.Reader
 import in.rcard.functional.portfolio.algebra.{ErrorsOr, PortfolioRepository, PortfolioService}
 
+import scala.runtime.Nothing$
+
 object PortfolioService extends PortfolioService[Portfolio, PortfolioId, ErrorsOr] {
 
   type Repository = PortfolioRepository[Portfolio, PortfolioId, ErrorsOr]
@@ -11,9 +13,18 @@ object PortfolioService extends PortfolioService[Portfolio, PortfolioId, ErrorsO
     Reader { repo =>
       for {
         pId <- PortfolioId.create(id)
-        _ <- repo.findById(pId)
+        maybePort <- repo.findById(pId)
+        _ <- Either.cond(maybePort.isEmpty, List(s"Portfolio $id already exists"), Nil) // XXX Mmm...
         portfolio <- Portfolio.emptyPortfolio(id)
         _ <- repo.store(portfolio)
       } yield portfolio
     }
+
+  override def sell(idPortfolio: String, stock: String, qty: Long): Reader[Repository, ErrorsOr[Portfolio]] = ???
+//    Reader { repo =>
+//      for {
+//        pId <- PortfolioId.create(idPortfolio)
+//        portfolio <- repo.findById(pId)
+//      } yield portfolio
+//    }
 }
